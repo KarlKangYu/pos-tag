ab78708aedf7e34bc5e357a1027a60fb589aac61
KarlKangYu <karl.kang@kikatech.com>
2018-08-02 17:35:50 +0800

    test

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import data_loader
 from text_cnn import TextCNN
 import sys
 
-def test(pos_file, neg_file, ckpt1, ckpt2, ckpt3, sequence_length=30, words_vocab_size=50000, tags_vocab_size=44, ensemble=True,
+def test(pos_file, neg_file, ckpts_num, ckpts_path, sequence_length=30, words_vocab_size=50000, tags_vocab_size=44, ensemble=True,
          deps_vocab_size=47, embedding_dim=300, filter_sizes="3,4,5", num_filters=128):
     # Data Preparation
     # ==================================================
@@ -36,110 +36,34 @@ def test(pos_file, neg_file, ckpt1, ckpt2, ckpt3, sequence_length=30, words_voca
 
             sess.run(tf.global_variables_initializer())
 
-            # ckpts_path = ckpts_path.strip().split('#')
-            # assert ckpts_num == len(ckpts_path)
-            #
-            # for i in range(ckpts_num):
-            #     ckpt_path = ckpts_path[i]
-            #     saver.restore(sess=sess, save_path=ckpt_path)
-            #     print("*" * 20 + "\nLoading The {} Model:\n")
-            #
-            #
-            #
-            #
+            ckpts_path = ckpts_path.strip().split('#')
+            assert ckpts_num == len(ckpts_path)
 
+            for i in range(ckpts_num):
+                ckpt_path = ckpts_path[i]
+                saver.restore(sess=sess, save_path=ckpt_path)
+                print("*" * 20 + "\nLoading The {} Model from {}:\n".format(i+1, ckpt_path))
 
+                feed_dict = {
+                    cnn.input_x: x,
+                    cnn.input_tags: tags,
+                    cnn.input_deps: deps,
+                    cnn.input_head: heads,
+                    cnn.input_y: y,
+                    cnn.dropout_keep_prob: 1.0
+                }
 
+                prediction, probability, accuracy = sess.run([cnn.predictions, cnn.probabilities, cnn.accuracy], feed_dict=feed_dict)
+                probabilities.append(probability)
 
-            saver.restore(sess=sess, save_path=ckpt1)
-
-            print("*" * 20 + "\nFirst Model:\n")
-
-            feed_dict = {
-                cnn.input_x: x,
-                cnn.input_tags: tags,
-                cnn.input_deps: deps,
-                cnn.input_head: heads,
-                cnn.input_y: y,
-                cnn.dropout_keep_prob: 1.0
-            }
-
-            predictions1, probability1, accuracy1 = sess.run([cnn.predictions, cnn.probabilities, cnn.accuracy], feed_dict=feed_dict)
-            probabilities.append(probability1)
-
-            count=0
-            for i in range(len(predictions1)):
-                if predictions1[i] == 1 and predictions1[i] == label[i]:
-                    count += 1
-            recall = count/neg_y
-
-            neg_pre1 = np.sum(predictions1 == 1)
-            print("In prediction, Negative number:", neg_pre1)
-            precision = count/neg_pre1
-
-            print("Accuracy:", accuracy1, "Recall:", recall, "Precision:", precision, "\n" + "*" * 20)
-
-            ######################################################################################################
-
-            saver.restore(sess=sess, save_path=ckpt2)
-
-            print("*" * 20 + "\nSecond Model:\n")
-
-            feed_dict = {
-                cnn.input_x: x,
-                cnn.input_tags: tags,
-                cnn.input_deps: deps,
-                cnn.input_head: heads,
-                cnn.input_y: y,
-                cnn.dropout_keep_prob: 1.0
-            }
-
-            predictions2, probability2, accuracy2 = sess.run([cnn.predictions, cnn.probabilities, cnn.accuracy],
-                                                             feed_dict=feed_dict)
-            probabilities.append(probability2)
-
-            count = 0
-            for i in range(len(predictions2)):
-                if predictions2[i] == 1 and predictions2[i] == label[i]:
-                    count += 1
-            recall = count / neg_y
-
-            neg_pre2 = np.sum(predictions2 == 1)
-            print("In prediction, Negative number:", neg_pre2)
-            precision = count / neg_pre2
-
-            print("Accuracy:", accuracy2, "Recall:", recall, "Precision:", precision, "\n" + "*" * 20)
-
-            #########################################################################################################
-
-            saver.restore(sess=sess, save_path=ckpt3)
-
-            print("*" * 20 + "\nThird Model:\n")
-
-            feed_dict = {
-                cnn.input_x: x,
-                cnn.input_tags: tags,
-                cnn.input_deps: deps,
-                cnn.input_head: heads,
-                cnn.input_y: y,
-                cnn.dropout_keep_prob: 1.0
-            }
-
-            predictions3, probability3, accuracy3 = sess.run([cnn.predictions, cnn.probabilities, cnn.accuracy],
-                                                             feed_dict=feed_dict)
-            probabilities.append(probability3)
-
-            count = 0
-            for i in range(len(predictions3)):
-                if predictions3[i] == 1 and predictions3[i] == label[i]:
-                    count += 1
-            recall = count / neg_y
-
-            neg_pre3 = np.sum(predictions3 == 1)
-            print("In prediction, Negative number:", neg_pre3)
-            precision = count / neg_pre3
-
-            print("Accuracy:", accuracy3, "Recall:", recall, "Precision:", precision, "\n" + "*" * 20)
+                count = 0
+                for i in range(len(prediction)):
+                    if prediction[i] == 1 and prediction[i] == label[i]:
+                        count += 1
+                recall = count / neg_y
+                precision = count / np.sum(prediction == 1)
+                print("Accuracy: {}, Recall:{}, Precision:{}".format(accuracy, recall, precision))
+                print("\n")
 
             if ensemble:
                 probabilities = np.array(probabilities)
@@ -158,12 +82,13 @@ def test(pos_file, neg_file, ckpt1, ckpt2, ckpt3, sequence_length=30, words_voca
                 print("Accuracy:", accuracy, "Recall:", recall, "Precision:", precision)
                 print("\n" + "*" * 20)
 
+
 if __name__ == "__main__":
     args = sys.argv
     pos_file = args[1]
     neg_file = args[2]
-    ckpt1 = args[3]
-    ckpt2 = args[4]
-    ckpt3 = args[5]
-    test(pos_file, neg_file, ckpt1, ckpt2, ckpt3)
+    ckpts_num = args[3]
+    ckpts_path = args[4]
+
+    test(pos_file, neg_file, ckpts_num, ckpts_path)
 
