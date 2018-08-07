cdba6d50c32dc6c3605c604d0112ec0a27b19bd3
KarlKangYu <karl.kang@kikatech.com>
2018-08-07 11:42:45 +0800

    change initializer

--- text_cnn.py
+++ text_cnn.py
@@ -21,7 +21,7 @@ class TextCNN(object):
         # Keeping track of l2 regularization loss (optional)
         l2_loss = tf.constant(0.0)
         #initializer = tf.contrib.layers.variance_scaling_initializer()
-        initializer = tf.keras.initializers.he_uniform()
+        initializer = tf.glorot_normal_initializer()
 
         # Embedding layer
         with tf.device('/cpu:0'), tf.name_scope("embedding_words"):
