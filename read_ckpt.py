71432bf28a9eedf9477db4568438d5e0042a2c04
KarlKangYu <karl.kang@kikatech.com>
2018-08-09 13:34:18 +0800

    test

--- read_ckpt.py
+++ read_ckpt.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     var_to_shape_map = reader.get_variable_to_shape_map()
     for key in var_to_shape_map:
         print("tensor_name: ", key)
-        print(reader.get_tensor(key)) # Remove this is you want to print only variable names
+        #print(reader.get_tensor(key)) # Remove this is you want to print only variable names
