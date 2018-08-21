afe0ecf59122811511f797fedc2a0ac620645552
KarlKangYu <karl.kang@kikatech.com>
2018-08-21 16:18:00 +0800

    pos tagging

--- pos_tagging.py
+++ pos_tagging.py
@@ -22,9 +22,22 @@ def pos_tagging(data_in, data_out):
                     f2.write(tag + " ")
                 f2.write("\n")
 
+def pos(data_in, data_out):
+    nlp = spacy.load("en")
+    with codecs.open(data_in, "r") as f1:
+        with codecs.open(data_out, 'w') as f2:
+            for line in f1.readlines():
+                line = line.strip()
+                f2.write(line + "#")
+                doc = nlp(line)
+                for token in doc:
+                    pos = token.pos_
+                    f2.write(pos + ' ')
+                f2.write("\n")
+
 
 if __name__ == "__main__":
     args = sys.argv
     data_in = args[1]
     data_out = args[2]
-    pos_tagging(data_in, data_out)
+    pos(data_in, data_out)
